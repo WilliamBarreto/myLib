4395b9514b57d015e2d8b8e65df08190c2100223
JonathanCabral <jpcabral@hotmail.com.br>
2017-11-13 00:41:12 -0200

    Criaçaõ da interface IStyleRepository.java

---
+++ codigo-fonte/mylib/src/main/java/br/com/estudos/mylib/repository/IStyleRepository.java
@@ -0,0 +1,10 @@
+package br.com.estudos.mylib.repository;
+
+import java.util.List;
+
+import br.com.estudos.mylib.entity.Style;
+
+public interface IStyleRepository {
+
+	public List<Style> listar();
+}
