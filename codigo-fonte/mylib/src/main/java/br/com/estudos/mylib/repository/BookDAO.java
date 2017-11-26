ba8de2a945a5b65122e8815995c5592b7227d85c
WilliamBarreto <bsi.william@gmail.com>
2017-11-26 20:10:48 -0200

    Corrigindo merge

--- codigo-fonte/mylib/src/main/java/br/com/estudos/mylib/repository/BookDAO.java
+++ codigo-fonte/mylib/src/main/java/br/com/estudos/mylib/repository/BookDAO.java
@@ -9,12 +9,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Root;
 
 import br.com.estudos.mylib.entity.Book;
-import br.com.estudos.mylib.entity.Style;
 
 @Stateless
 @TransactionAttribute(TransactionAttributeType.MANDATORY)
@@ -28,8 +25,6 @@ public class BookDAO implements IBookRepository {
 		CriteriaBuilder cb = entityManger.getCriteriaBuilder();
 		CriteriaQuery<Book> cq = cb.createQuery(Book.class);
 		Root<Book> root = cq.from(Book.class);
-		Join<Book, Style> join = root.join("styles",JoinType.LEFT);
-		cq.orderBy(cb.asc(join.get("style")));
 		cq.orderBy(cb.asc(root.<String>get("book")));
 
 		return entityManger.createQuery(cq).getResultList();
